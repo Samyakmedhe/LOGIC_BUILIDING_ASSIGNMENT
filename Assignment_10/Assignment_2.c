09d73a4d9753fb78335422727630ba89d2dd165d
Samyak Kailas Medhe <152603493+Samyakmedhe@users.noreply.github.com>
2025-05-16 01:10:24 +0530

    Add files via upload

---
+++ Assignment_10/Assignment_2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int CountOdd(int iNo)
+{
+    int iDigit = 0 ;
+    int iCnt = 0;
+    if(iNo < 0 )
+    {
+        iNo= -iNo;
+    }
+
+   while(iNo!= 0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit % 2 != 0)
+        {
+            iCnt = iCnt + 1;
+        }
+        iNo = iNo /10;
+    }
+    return iCnt;
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+    printf("Enter number to Count Odd Digit : ");
+    scanf("%d",&iValue);
+
+    iRet = CountOdd(iValue);
+    printf("Output: %d\n",iRet);
+    return 0 ;
+}
