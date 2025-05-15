366cfef22df20267d2fa4cafdf90046369beeb66
Samyak Kailas Medhe <152603493+Samyakmedhe@users.noreply.github.com>
2025-05-16 01:15:58 +0530

    Add files via upload

---
+++ Assignment__10/Assignment_3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int CountRange(int iNo)
+{
+    int iDigit = 0 ;
+    int iCnt = 0;
+    if(iNo < 0 )
+    {
+        iNo= -iNo;
+    }
+
+   while(iNo!= 0)
+    {
+        iDigit = iNo % 10;
+        if((iDigit >=3) &&(iDigit <= 7))
+        {
+            iCnt = iCnt + 1;
+        }
+        iNo = iNo /10;
+    }
+    return iCnt;
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+    printf("Enter number to Count between 7 and 3 : ");
+    scanf("%d",&iValue);
+
+    iRet = CountRange(iValue);
+    printf("Output: %d\n",iRet);
+    return 0 ;
+}
